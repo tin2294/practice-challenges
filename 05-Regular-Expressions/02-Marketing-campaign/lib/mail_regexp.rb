ca2c323b12b1967cd27062ec21f0ec8084c8b252
Ting Ting Chen <tin2294@gmail.com>
2022-01-15 14:09:38 +0100

    challenge 2 part 1

--- 05-Regular-Expressions/02-Marketing-campaign/lib/mail_regexp.rb
+++ 05-Regular-Expressions/02-Marketing-campaign/lib/mail_regexp.rb
@@ -1,14 +1,25 @@
 def valid?(email)
   # TODO: return true if the email is valid, false otherwise
+  email.match?(/^.+@.+\.[a-z]{3}/) || email.match?(/^.+@.+\.[a-z]{2}/)
 end
 
 def clean_database(emails)
   # TODO: return an array with the valid emails only
+  emails.select { |email| valid?(email) }
 end
 
 def group_by_tld(emails)
   # TODO: return a Hash with emails grouped by TLD
+  emails.each do |email|
+    pattern = /^(?<first_part>.+\.) (?<last_part>[a-z]+$)/
+    match_data = email.match(pattern)
+    puts match_data[:first_part]
+    # if email
+    # end
+  end
 end
+emails = ["kevin@yahoo.fr", "edward@gmail.fr", "julien@mdn.com", "dimitri@berlin.de"]
+group_by_tld(emails)
 
 def compose_mail(email)
   # TODO: return a Hash with username, domain and tld extracted from email
