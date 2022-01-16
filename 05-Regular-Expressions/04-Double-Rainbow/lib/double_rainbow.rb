db4e6f3dccf08a7ae1f7eeba606e85110798f2d6
Ting Ting Chen <tin2294@gmail.com>
2022-01-16 01:34:27 +0100

    challenge 4

--- 05-Regular-Expressions/04-Double-Rainbow/lib/double_rainbow.rb
+++ 05-Regular-Expressions/04-Double-Rainbow/lib/double_rainbow.rb
@@ -1,41 +1,60 @@
 def word_contains_two_p(sentence)
   # TODO: return the first word in `sentence` containing two 'p's
-  regexp = /\b\w*p\w*p\w*\b/
-  sentence.match(regexp)[0]
+  result = sentence.split.map(&:to_s)
+  result.find { |word| word.match?(/.*p.*p.*/) }
 end
+
+# secret_message = "Reveal the logos' colors:\
+#  Elegant shapes, some have evolved to a very iconized style.\
+#  Definitely a vivid color scheme with bright orange and shiny yellow,\
+#  many shades of blue, oscillating between purple and indigo! but not much green"
+
+# word_contains_two_p(secret_message)
 
 def word_before_exclamation_mark(sentence)
   # TODO: return the first word located before an exclamation mark
   # Hint: you may want to use `()` groups!
-  return sentence[/\w+?(?=!)/]
+  result = sentence.split.map(&:to_s)
+  word = result.find { |word| word.match?(/.+!/) }
+  word.gsub('!', '')
 end
 
-test_string = "you? me!"
-p word_before_exclamation_mark(test_string)
+# test_string = "you? me!"
+# p word_before_exclamation_mark(test_string)
 
 def four_letter_word(sentence)
   # TODO: return the first four-letter word starting with 'b', followed with a comma `,`
-  return sentence[/\bb.{3}\b/]
+  result = sentence.split.map(&:to_s)
+  word = result.find { |word| word.match?(/b.+,/) }
+  word.gsub(',', '')
 end
 
 def last_five_letters(sentence)
+  return sentence.match(/\w{5}\z/).to_s
   # TODO: return the last five letters of the sentence
-  return sentence[/.{5}$/]
 end
 
 def word_contains_ll(sentence)
   # TODO: return the first word containing two consecutive 'l'
-  return sentence[/\b\w*ll\w*\b/]
+  result = sentence.split.map(&:to_s)
+  word = result.find { |word| word.match?(/.*ll.*/) }
+  if word.match?(/[^a-zA-Z]/)
+    word.gsub(word.match(/[^a-zA-Z]/).to_s, "")
+  else
+    word
+  end
 end
 
 def six_letter_word(sentence)
   # TODO: return the first six-letter word, composed of letters from 'a' to 'r'
-  return sentence[/\b[a-r]{6}\b/]
+  result = sentence.split.map(&:to_s)
+  result.find { |word| word.match?(/[a-rA-R]{6}\b/) }
 end
 
 def all_capital_letters(sentence)
   # TODO: return all the capital letters as a string
   # Hint: use `scan` to get all the occurences then `join` them
-  arr_word = sentence.scan(/[A-Z]/)
-  return arr_word.join
+  sentence.scan(/[A-Z]/).join
 end
+test_string = "ManY COLORS"
+p all_capital_letters(test_string)
