792f44b1eb4e2b2ad0ff4cdf1648a81b6d0d163f
Ting Ting Chen <tin2294@gmail.com>
2022-01-16 01:43:52 +0100

    calculator

--- 05-Regular-Expressions/Reboot-01-Calculator/lib/interface.rb
+++ 05-Regular-Expressions/Reboot-01-Calculator/lib/interface.rb
@@ -1,1 +1,28 @@
 # TODO: you can build your calculator program here!
+def calculate(operation, first_number, second_number)
+  case operation
+  when '+'
+    first_number + second_number
+  when '-'
+    first_number - second_number
+  when '*'
+    first_number * second_number
+  when '/'
+    first_number / second_number
+  end
+end
+
+def calculator
+  puts 'Enter your first number'
+  p '>'
+  first_number = gets.chomp.to_i
+  puts 'Enter a second number'
+  p '>'
+  second_number = gets.chomp.to_i
+  puts 'Choose operation [+][-][*][/]'
+  p '>'
+  operation = gets.chomp
+  puts "Result: #{calculate(operation, first_number, second_number)}"
+end
+
+calculator
