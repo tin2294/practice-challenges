e230eb276486f9e0ec096e005e0b12279bd08580
Ting Ting Chen <tin2294@gmail.com>
2022-01-18 23:36:35 +0100

    text analyzer

--- 06-Parsing/Optional-01-Text-Analyzer/lib/text_analyzer.rb
+++ 06-Parsing/Optional-01-Text-Analyzer/lib/text_analyzer.rb
@@ -4,12 +4,31 @@ end
 
 def analyze(text)
   # TODO: should analyze the text, and return the result hash with all features
-  {
+  hash = {
     character_count: text.length,
     character_count_excluding_spaces: text.gsub(/\s/, '').length,
-    line_count: text.scan(/$/, '').length
+    line_count: text.scan(/$/).length,
+    word_count: text.scan(/\b/).length,
+    sentence_count: text.scan(/\./).length
   }
+  return hash
 end
+
+
+    # paragraph_count:
+    # if text.scan(/\n/).length.zero?
+    #   1.0
+    # else
+    #   text.scan(/\n/).length.to_f
+    # end,
+    # average_words_per_sentence: text.scan(/\b/).length / text.scan(/\./).length,
+    # average_sentences_per_paragraph: text.scan(/\./).length / (
+    # if text.scan(/\n/).length.zero?
+    #   1.0
+    # else
+    #   text.scan(/\n/).length.to_f
+    # )
+    # end
 
 test_string = "I love programming."
 
