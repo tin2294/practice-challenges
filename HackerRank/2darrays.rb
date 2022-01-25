ae20aa2693c80c089ec2d9fbcee16065c9b60b47
Ting Ting Chen <tin2294@gmail.com>
2022-01-25 20:38:40 +0100

    hourglass

---
+++ HackerRank/2darrays.rb
@@ -0,0 +1,15 @@
+arr = [
+  [1,1,1,0,0,0]
+  [0,1,0,0,0,0]
+  [1,1,1,0,0,0]
+  [0,0,2,4,4,0]
+  [0,0,0,2,0,0]
+  [0,0,1,2,4,0]
+]
+
+def hourglassSum(arr)
+    # Write your code here
+
+end
+
+p hourglassSum(arr)
