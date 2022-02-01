8e9003d44b1fce9d218195adec317f7d8a30b919
Ting Ting Chen <tin2294@gmail.com>
2022-02-01 02:46:15 +0100

    style exercise 2

--- ritmo/exercise2.rb
+++ ritmo/exercise2.rb
@@ -1,11 +1,9 @@
-tomato_slices = 16
-cheese_slices = 7
-
+# Assignment:
 # return [total_big, total_small]
-
 # 1 big = 4 tomato, 1 cheese
 # 1 small = 2 tomato, 1 cheese
 
+# Assumptions:
 # tomato has to be greater than cheese slices
 
 def making_sandwiches(tomato, cheese)
@@ -15,10 +13,13 @@ def making_sandwiches(tomato, cheese)
     (1..total_sandwiches).each do |count|
       big_tomatoes = count * 4
       small_tomatoes = (total_sandwiches - count) * 2
-      result = [count, (total_sandwiches - count)] if (tomato - (big_tomatoes + small_tomatoes)) == 0
+      result = [count, (total_sandwiches - count)] if (tomato - (big_tomatoes + small_tomatoes)).zero?
     end
   end
-  return result
+  result
 end
+
+tomato_slices = 16
+cheese_slices = 7
 
 p making_sandwiches(tomato_slices, cheese_slices)
