64f4588b5a3620f3ac6ef013bc03fba75313a884
Ting Ting Chen <tin2294@gmail.com>
2022-02-07 20:58:59 +0100

    linked lists class

--- misc./Leetcode/swap_nodes.rb
+++ misc./Leetcode/swap_nodes.rb
@@ -1,26 +1,72 @@
-def swap_pairs(head)
-  head_final = []
-  indices = []
-  head.each do |number|
-    index = head.index(number)
-    if index < (head.length - 1)
-      if indices.include?(index)
-      else
-        indices << index
-        indices << index + 1
-        head_final << head[index + 1]
-        head_final << number
-      end
-    elsif (head.length % 2) != 0
-      head_final << head.last
+# Definition for singly-linked list.
+class ListNode
+    attr_accessor :val, :next
+    def initialize(val = 0, _next = nil)
+        @val = val
+        @next = _next
     end
+end
+# @param {ListNode} head
+# @return {ListNode}
+
+def swap_pairs(head)
+  dummy = ListNode.new(nil)
+  dummy.next = head
+  prev = dummy
+  while !head.nil? && !head.next.nil?
+    nxt = head.next
+    head.next = nxt.next
+    nxt.next = head
+    prev.next = nxt
+    prev = head
+    head = head.next
   end
-  head_final
+  return dummy.next
 end
 
-head = [1,2,3,4]
+head = ListNode.new(1, ListNode.new(2, ListNode.new(3, ListNode.new(4))))
 
 p swap_pairs(head)
 
-# linked lists?
-#
+# def swap_pairs(head)
+#   head_final = []
+#   indices = []
+#   head.each do |number|
+#     index = head.index(number)
+#     if index < (head.length - 1)
+#       if indices.include?(index)
+#       else
+#         indices << index
+#         indices << index + 1
+#         head_final << head[index + 1]
+#         head_final << number
+#       end
+#     elsif (head.length % 2) != 0
+#       head_final << head.last
+#     end
+#   end
+#   head_final
+# end
+
+# head = [1,2,3,4]
+
+# p swap_pairs(head)
+
+
+
+# buena
+    # return [] if head.nil?
+    # return head if head.next.nil?
+
+    # first = head
+    # second = head.next
+    # p first.val.to_s + " " + second.val.to_s
+    # third = head.next.next
+    # p third
+
+    # head = second
+    # head.next = first
+    # head.next.next = third
+    # head.next.next = swap_pairs(head.next.next) unless head.next.next.nil?
+
+    # return head
