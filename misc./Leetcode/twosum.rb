a4dd24d1f319830e741d07a33dc07b6a3606ed3f
Ting Ting Chen <tin2294@gmail.com>
2022-02-08 01:47:51 +0100

    two sum

--- misc./Leetcode/twosum.rb
+++ misc./Leetcode/twosum.rb
@@ -1,11 +1,23 @@
+# SUPER LONG RUNNING
+# def two_sum(nums, target)
+#   result = []
+#   nums.each_with_index do |num, index|
+#     ((index + 1)..(nums.length - 1)).each do |mini_index|
+#       result << [index, mini_index] if nums[mini_index] + num == target
+#     end
+#   end
+#   return result.flatten
+# end
+
+# super cool solution using hash
 def two_sum(nums, target)
-  result = []
-  nums.each_with_index do |num, index|
-    ((index + 1)..(nums.length - 1)).each do |mini_index|
-      result << [index, mini_index] if nums[mini_index] + num == target
+  dict = {}
+  nums.each_with_index do |n, i|
+    if dict[target - n]
+      return dict[target - n], i
     end
+    dict[n] = i
   end
-  return result.flatten
 end
 
 nums = [3,2,4]
